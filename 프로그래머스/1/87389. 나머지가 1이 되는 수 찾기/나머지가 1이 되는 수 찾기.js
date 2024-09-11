f7a0e57a98a8fb6f3995cb978b92a22f0ccccd79
GYURI <97436315+9yurilee@users.noreply.github.com>
2024-09-11 16:11:27 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 8.75 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -2,10 +2,10 @@ function solution(n) {
     let x = 0;
     
     while (n) {
-        if(n % x === 1) {
-            return x;
-        } 
-        
-        x++;
+        if(n % x !== 1) {
+            x++;
+            continue;
+        }    
+        return x;
     }
 }
