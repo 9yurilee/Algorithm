1a16a08bdf539f9b30e482f81fe7f9bf679e234b
GYURI <97436315+9yurilee@users.noreply.github.com>
2024-09-20 10:31:22 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -0,0 +1,13 @@
+function solution(array, commands) {
+    let answer = [];
+    
+    for(let i = 0; i < commands.length; i++) {
+        const slicedArr = array.slice(commands[i][0] - 1, commands[i][1])
+    
+        slicedArr.sort((a, b) => a - b);
+                
+        answer.push(slicedArr[commands[i][2] - 1]);
+    }
+    
+    return answer;
+}
