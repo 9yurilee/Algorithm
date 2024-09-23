27167438ccafff3e9f88bd0c8009e3d10a969e6f
GYURI <97436315+9yurilee@users.noreply.github.com>
2024-09-23 19:07:51 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -2,6 +2,7 @@ function solution(num1, num2) {
   
     // 최대공약수
     const getGCD = (a, b) => {
+        // a와 b의 최대공약수는 b와 a % b의 최대공약수와 같다(공약수의 성질)
         while (b !== 0) {
             let tmp = b; 
             
@@ -13,8 +14,8 @@ function solution(num1, num2) {
     
     // 최소공배수
     const getLCM = (x, y) => {
-        return Math.abs(x * y) / getGCD(num1, num2)
+        return Math.abs(x * y) / getGCD(num1, num2);
     }
     
-    return [getGCD(num1, num2), getLCM(num1, num2)]
+    return [getGCD(num1, num2), getLCM(num1, num2)];
 }
